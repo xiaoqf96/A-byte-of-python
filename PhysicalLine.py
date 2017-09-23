75aa2cd7653a5da10f45fcaea5450c5456900d27
xiaoqf96 <xiaoqf776@gmail.com>
2017-09-23 20:21:36 +0800

    Add files via upload

---
+++ PhysicalLine.py
@@ -0,0 +1,14 @@
+#所谓物理行（Physical Line）是你在编写程序时 你所看到 的内容。
+# 所谓逻辑行（LogicalLine）是 Python 所看到 的单个语句。
+# Python 会假定每一 物理行 会对应一个  逻辑行。
+
+
+i = 5;print(i);
+#建议你对于每一行物理行最多只写入一行逻辑行.不建议出现分号！
+
+
+#如果一行代码特别长,可以用\拆分——显示行连接（Explicit Line Joining）
+s = 'This is a string. \
+This continues the string.'
+print(s)
+
